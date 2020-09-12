b3a1b057f149c797a0755cc52069e58c36c0ed1a
Daniel Bungert <gewinnspiel@daniel-bungert.de>
2020-09-12 22:43:44 +0200

    Powershell Filme: lower file size limit

--- Powershell/check_Filme_ignore_already_done.ps1
+++ Powershell/check_Filme_ignore_already_done.ps1
@@ -3,7 +3,7 @@ $env:Path += "C:\Users\gewin\bin\ffmpeg-4.2-win64-static\bin"
 
 Get-ChildItem -File | 
 Where-Object Name -NotLike "*-.*" | 
-Where-Object Length -GT 900000000 | 
+Where-Object Length -GT 300000000 | 
 ForEach-Object {
     $NAME = $_.Name
     $NAME
