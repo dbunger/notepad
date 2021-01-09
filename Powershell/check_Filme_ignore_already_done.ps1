aa2e717152c003424901f5242fbc15f1e88e36d3
Daniel Bungert <gewinnspiel@daniel-bungert.de>
2021-01-09 10:43:15 +0100

    more PS

--- Powershell/check_Filme_ignore_already_done.ps1
+++ Powershell/check_Filme_ignore_already_done.ps1
@@ -2,7 +2,7 @@
 $env:Path += "C:\Users\gewin\bin\ffmpeg-4.2-win64-static\bin"
 
 Get-ChildItem -File | 
-Where-Object Name -NotLike "*-.*" | 
+Where-Object Name -NotLike "*_.*" | 
 Where-Object Length -GT 300000000 | 
 ForEach-Object {
     $NAME = $_.Name
@@ -12,7 +12,7 @@ ForEach-Object {
         Write-Output "Already checked ..."
     }
     else {
-        $TMPNAME = $NAME -replace "\.", "--."
+        $TMPNAME = $NAME -replace "\.", "_."
         $TMPNAME
         ffmpeg.exe -i "$NAME" -codec copy "$TMPNAME" *>> $LOGNAME
     }
